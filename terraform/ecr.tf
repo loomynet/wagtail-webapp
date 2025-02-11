fc55ab4db9d31ab0f266b83f574e9e5532f2403a
Andrejs Jakuņins <95055364+loomynet@users.noreply.github.com>
2025-02-11 23:04:54 +0200

    Update ecr.tf

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,8 @@
 resource "aws_ecr_repository" "wagtail_repo" {
   name = var.ecr_repository_name
+
+  lifecycle {
+    prevent_destroy = true  # Prevent accidental deletion
+    ignore_changes  = [image_tag_mutability, image_scanning_configuration]  # Ignore changes in ECR settings
+  }
 }
