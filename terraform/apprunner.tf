b67e1d379192200de8d3f9054f5d3b60c428b0b7
Andrejs Jakunins <andrejsjakunins@gmail.com>
2025-02-12 10:29:39 +0200

    Added healt_check_configuration in apprunner.tf

--- terraform/apprunner.tf
+++ terraform/apprunner.tf
@@ -9,18 +9,19 @@ resource "aws_apprunner_service" "wagtail_service" {
     image_repository {
       image_configuration {
         port = "80"
-        # runtime_environment_variables = {
-        #   DJANGO_SETTINGS_MODULE = "mysite.settings"
-        # }
       }
       image_identifier      = "${aws_ecr_repository.wagtail_repo.repository_url}:${var.image_tag}"
       image_repository_type = "ECR"
     }
   }
-
+  
   instance_configuration {
     cpu    = "1024"
     memory = "2048"
+  }
+
+  health_check_configuration {
+    protocol = "HTTP"
   }
 
   auto_scaling_configuration_arn = aws_apprunner_auto_scaling_configuration_version.auto_scaling.arn
