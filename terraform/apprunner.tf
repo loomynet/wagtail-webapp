115e25828361bffd90690879fa94d7679e229e6b
Andrejs Jakuņins <95055364+loomynet@users.noreply.github.com>
2025-02-12 15:46:17 +0200

    Update apprunner.tf

--- terraform/apprunner.tf
+++ terraform/apprunner.tf
@@ -10,8 +10,8 @@ resource "aws_apprunner_service" "wagtail_service" {
       image_configuration {
         port = "8000"
       }
-      image_identifier      = "597765856364.dkr.ecr.eu-west-1.amazonaws.com/nginx-test:latest"
-      # "${aws_ecr_repository.wagtail_repo.repository_url}:${var.image_tag}"
+      image_identifier      = "${aws_ecr_repository.wagtail_repo.repository_url}:${var.image_tag}" 
+      # image_indentifier   = "597765856364.dkr.ecr.eu-west-1.amazonaws.com/nginx-test:latest"
       image_repository_type = "ECR"
     }
   }
