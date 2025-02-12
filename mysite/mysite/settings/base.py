2370eea1ff410fa953f10a988570329561dd8796
Andrejs Jakunins <andrejsjakunins@gmail.com>
2025-02-12 10:47:48 +0200

    Add AWS App Runner to ALLOWED_HOSTS in base.py

--- mysite/mysite/settings/base.py
+++ mysite/mysite/settings/base.py
@@ -173,6 +173,8 @@ WAGTAILSEARCH_BACKENDS = {
     }
 }
 
+ALLOWED_HOSTS = [".awsapprunner.com"]
+
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 WAGTAILADMIN_BASE_URL = "http://example.com"
