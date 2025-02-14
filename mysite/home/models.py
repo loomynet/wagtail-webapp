7be43cac39182cbcfff38850afc4e07696036f6b
Andrejs Jakunins <andrejsjakunins@gmail.com>
2025-02-14 09:53:52 +0200

    Remove unused fields and comments from HomePage model and template

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,17 +1,6 @@
-# from django.db import models
-
-# from wagtail.models import Page
-
-
-# class HomePage(Page):
-#     pass
-
 from django.db import models
 
 from wagtail.models import Page
-from wagtail.fields import RichTextField
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
-
-    content_panels = Page.content_panels + ["body"]
+    pass
